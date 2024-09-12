a604a79fa6439c7f69bfd76a9de94d3d378fb0c3
martins-pedro23 <martinsdeoliveira.pedro2@gmail.com>
2024-09-11 21:14:37 -0300

    fix: remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,10 +28,7 @@ import {
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
-  CardHeader,
-  CardTitle,
 } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
