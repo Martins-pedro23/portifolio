3d0384ff6a249dd3b2c18c08ab3c49c6b1994675
martins-pedro23 <martinsdeoliveira.pedro2@gmail.com>
2024-09-13 22:08:18 -0300

    feat: changed the cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ import BackgroundVideo from "next-video/background-video";
 import logoAgriloggi from "./public/images/agriloggiLogo.png";
 import logoEstudeMelhor from "./public/images/logoEstudeMelhor.svg";
 import labMaker from "./public/images/LabIFMaker4.png";
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { CardContent, CardFooter } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
 import { Checkbox } from "@/components/ui/checkbox";
